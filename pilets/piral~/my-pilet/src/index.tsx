68d4596be4eca865ac75a394124a7288df37a066
Lukas Eiter <lukas.eiter@casablanca.at>
2022-01-14 11:51:34 +0100

    rebuild my-pilet

---
+++ pilets/piral~/my-pilet/src/index.tsx
@@ -0,0 +1,12 @@
+﻿import { PiletApi } from '../../shells/my-app/dist/emulator/my-app-0.0.1.tgz';
+import * as Blazor from './blazor.codegen';
+import './my-pilet.styles.css';
+
+export function setup(app: PiletApi) {
+    Blazor.initPiralBlazorApi(app);
+    Blazor.registerDependencies(app);
+    Blazor.registerOptions(app);
+    Blazor.registerPages(app);
+    Blazor.registerExtensions(app);
+    Blazor.setupPilet(app);
+}
